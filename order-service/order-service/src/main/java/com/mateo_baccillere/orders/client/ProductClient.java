407923866cad7e5a33f52c5c2bd8bfbb766b159a
Mateo Baccillere <mateobaccillere1@gmail.com>
2026-03-25 15:06:47 -0300

    add product service client integration

--- order-service/order-service/src/main/java/com/mateo_baccillere/orders/client/ProductClient.java
+++ order-service/order-service/src/main/java/com/mateo_baccillere/orders/client/ProductClient.java
@@ -1,2 +1,31 @@
-package com.mateo_baccillere.orders.client;public class ProductClient {
+package com.mateo_baccillere.orders.client;
+
+import com.mateo_baccillere.orders.dto.ProductDetailsResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientResponseException;
+
+@Component
+public class ProductClient {
+
+    private final RestClient productRestClient;
+
+    public ProductClient(@Qualifier("productRestClient") RestClient productRestClient) {
+        this.productRestClient = productRestClient;
+    }
+
+    public ProductDetailsResponse getProductById(Long productId) {
+        try {
+            return productRestClient.get()
+                    .uri("/api/products/{id}", productId)
+                    .retrieve()
+                    .body(ProductDetailsResponse.class);
+        } catch (RestClientResponseException ex) {
+            if (ex.getStatusCode().value() == 404) {
+                return null;
+            }
+            throw ex;
+        }
+    }
 }
