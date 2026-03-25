1cb6b42ef912b653f41e1c3098f4cef83306ccf4
Mateo Baccillere <mateobaccillere1@gmail.com>
2026-03-25 10:43:34 -0300

    add product-service skeleton

---
+++ product-service/src/main/java/com/mateo_baccillere/products/entity/Product.java
@@ -0,0 +1,37 @@
+package com.mateo_baccillere.products.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private Boolean active;
+}
