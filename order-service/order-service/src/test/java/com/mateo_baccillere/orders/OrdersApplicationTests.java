9e8ff4f9fcf9f919feaad316b5d190f230fc8385
Mateo Baccillere <mateobaccillere1@gmail.com>
2026-03-27 14:05:37 -0300

    fix OrderApplicationTest

--- order-service/order-service/src/test/java/com/mateo_baccillere/orders/OrdersApplicationTests.java
+++ order-service/order-service/src/test/java/com/mateo_baccillere/orders/OrdersApplicationTests.java
@@ -3,7 +3,7 @@ package com.mateo_baccillere.orders;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"product-service.base-url=http://localhost:8081" })
 class OrdersApplicationTests {
 
 	@Test
